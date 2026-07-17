4c21471216c304d43d48863b3031e93177d9ad8f
Aitthiphon-dev123 <14756@phanthong.ac.th>
2026-07-17 14:11:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("Portfolio Ready");
+
+// แสดงข้อความต้อนรับ
+window.addEventListener("load", function () {
+    console.log("ยินดีต้อนรับสู่เว็บไซต์ Portfolio");
+});
+
+// ไฮไลต์เมนูหน้าปัจจุบัน
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    if (link.href === window.location.href) {
+        link.style.color = "#38BDF8";
+        link.style.fontWeight = "600";
+    }
+});
